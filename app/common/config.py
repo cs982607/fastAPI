400ebe49bb5df3953a369f3c98904cd7d3ee3ef9
정현석 <cs982606@gmail.com>
2022-04-06 13:17:19 +0900

    config 변경

--- app/common/config.py
+++ app/common/config.py
@@ -13,30 +13,17 @@ class Config:
     BASE_DIR: str = base_dir
     DB_POOL_RECYCLE: int = 900
     DB_ECHO: bool = True
-    DEBUG: bool = False
-    TEST_MODE: bool = False
-    DB_URL: str = environ.get("DB_URL", "mysql+pymysql://travis@localhost/notification_api?charset=utf8mb4")
 
 
 @dataclass
 class LocalConfig(Config):
-    TRUSTED_HOSTS = ["*"]
-    ALLOW_SITE = ["*"]
+    PROJ_RELOAD: bool = True
     DEBUG: bool = True
 
 
 @dataclass
 class ProdConfig(Config):
-    TRUSTED_HOSTS = ["*"]
-    ALLOW_SITE = ["*"]
-
-
-@dataclass
-class TestConfig(Config):
-    DB_URL: str = "mysql+pymysql://travis@localhost/notification_test?charset=utf8mb4"
-    TRUSTED_HOSTS = ["*"]
-    ALLOW_SITE = ["*"]
-    TEST_MODE: bool = True
+    PROJ_RELOAD: bool = False
 
 
 def conf():
@@ -44,7 +31,7 @@ def conf():
     환경 불러오기
     :return:
     """
-    config = dict(prod=ProdConfig, local=LocalConfig, test=TestConfig)
+    config = dict(prod=ProdConfig(), local=LocalConfig())
     return config[environ.get("API_ENV", "local")]()
 
 
